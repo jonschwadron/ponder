cf6bffab57e619faf732a576b94c43fff9138857
jonschwadron <jonschwadron@gmail.com>
2015-06-28 23:00:31 +0000

    removed log files

--- static/javascripts/authentication/services/authentication.service.js
+++ static/javascripts/authentication/services/authentication.service.js
@@ -4,13 +4,13 @@
 */
 (function () {
   'use strict';
-
+  
   angular
-    .module('ponder.authentication.services')
-    .factory('Authentication', Authentication);
-
+  .module('ponder.authentication.services')
+  .factory('Authentication', Authentication);
+  
   Authentication.$inject = ['$cookies', '$http'];
-
+  
   /**
   * @namespace Authentication
   * @returns {Factory}
@@ -29,13 +29,12 @@
       getAuthenticatedAccount: getAuthenticatedAccount,
       setAuthenticatedAccount: setAuthenticatedAccount
     };
-
-    return Authentication;
-
+    
+    return Authentication;  
   }
-
+  
   ////////////////////
-
+  
   /**
   * @name register
   * @desc Try to register a new user
@@ -52,10 +51,8 @@
       email: email
     });
   }
-
-
-
- /**
+  
+  /**
   * @name login
   * @desc Try to log in with email `email` and password `password`
   * @param {string} email The email entered by the user
@@ -67,57 +64,57 @@
     return $http.post('/api/v1/auth/login/', {
       email: email, password: password
     }).then(loginSuccessFn, loginErrorFn);
-
-  /**
-   * @name loginSuccessFn
-   * @desc Set the authenticated account and redirect to index
-   */
-  function loginSuccessFn(data, status, headers, config) {
-    Authentication.setAuthenticatedAccount(data.data);
-
-    window.location = '/';
+    
+    /**
+    * @name loginSuccessFn
+    * @desc Set the authenticated account and redirect to index
+    */
+    function loginSuccessFn(data, status, headers, config) {
+      Authentication.setAuthenticatedAccount(data.data);
+      
+      window.location = '/';
+    }
+    
+    /**
+    * @name loginErrorFn
+    * @desc Log "Epic failure!" to the console
+    */
+    function loginErrorFn(data, status, headers, config) {
+      console.error('Epic failure!');
+    }
   }
-
+  
+  
   /**
-   * @name loginErrorFn
-   * @desc Log "Epic failure!" to the console
-   */
-  function loginErrorFn(data, status, headers, config) {
-    console.error('Epic failure!');
-  }
-}
-
-
-/**
- * @name logout
- * @desc Try to log the user out
- * @returns {Promise}
- * @memberOf thinkster.authentication.services.Authentication
- */
-function logout() {
-  return $http.post('/api/v1/auth/logout/')
+  * @name logout
+  * @desc Try to log the user out
+  * @returns {Promise}
+  * @memberOf thinkster.authentication.services.Authentication
+  */
+  function logout() {
+    return $http.post('/api/v1/auth/logout/')
     .then(logoutSuccessFn, logoutErrorFn);
-
-  /**
-   * @name logoutSuccessFn
-   * @desc Unauthenticate and redirect to index with page reload
-   */
-  function logoutSuccessFn(data, status, headers, config) {
-    Authentication.unauthenticate();
-
-    window.location = '/';
+    
+    /**
+    * @name logoutSuccessFn
+    * @desc Unauthenticate and redirect to index with page reload
+    */
+    function logoutSuccessFn(data, status, headers, config) {
+      Authentication.unauthenticate();
+      
+      window.location = '/';
+    }
+    
+    /**
+    * @name logoutErrorFn
+    * @desc Log "Epic failure!" to the console
+    */
+    function logoutErrorFn(data, status, headers, config) {
+      console.error('Epic failure!');
+    }
   }
-
-  /**
-   * @name logoutErrorFn
-   * @desc Log "Epic failure!" to the console
-   */
-  function logoutErrorFn(data, status, headers, config) {
-    console.error('Epic failure!');
-  }
-}
-
-
+  
+  
   /**
   * @name getAuthenticatedAccount
   * @desc Return the currently authenticated account
@@ -125,45 +122,46 @@ function logout() {
   * @memberOf ponder.authentication.services.Authentication
   */
   function getAuthenticatedAccount() {
-   if (!$cookies.authenticatedAccount) {
-     return;
-   }
-
-   return JSON.parse($cookies.authenticatedAccount);
- }
-
-
-/**
- * @name isAuthenticated
- * @desc Check if the current user is authenticated
- * @returns {boolean} True is user is authenticated, else false.
- * @memberOf ponder.authentication.services.Authentication
- */
-function isAuthenticated() {
-  return !!$cookies.authenticatedAccount;
-}
-
-
-/**
- * @name setAuthenticatedAccount
- * @desc Stringify the account object and store it in a cookie
- * @param {Object} user The account object to be stored
- * @returns {undefined}
- * @memberOf ponder.authentication.services.Authentication
- */
-function setAuthenticatedAccount(account) {
-  $cookies.authenticatedAccount = JSON.stringify(account);
-} 
-
-
-/**
- * @name unauthenticate
- * @desc Delete the cookie where the user object is stored
- * @returns {undefined}
- * @memberOf ponder.authentication.services.Authentication
- */
-function unauthenticate() {
-  delete $cookies.authenticatedAccount;
-}
-
+    if (!$cookies.authenticatedAccount) {
+      return;
+    }
+    
+    return JSON.parse($cookies.authenticatedAccount);
+  }
+  
+  
+  /**
+  * @name isAuthenticated
+  * @desc Check if the current user is authenticated
+  * @returns {boolean} True is user is authenticated, else false.
+  * @memberOf ponder.authentication.services.Authentication
+  */
+  function isAuthenticated() {
+    return !!$cookies.authenticatedAccount;
+  }
+  
+  
+  /**
+  * @name setAuthenticatedAccount
+  * @desc Stringify the account object and store it in a cookie
+  * @param {Object} user The account object to be stored
+  * @returns {undefined}
+  * @memberOf ponder.authentication.services.Authentication
+  */
+  function setAuthenticatedAccount(account) {
+    $cookies.authenticatedAccount = JSON.stringify(account);
+  }
+  
+  
+  /**
+  * @name unauthenticate
+  * @desc Delete the cookie where the user object is stored
+  * @returns {undefined}
+  * @memberOf ponder.authentication.services.Authentication
+  */
+  function unauthenticate() {
+    delete $cookies.authenticatedAccount;
+  }
+  
 })();
+
