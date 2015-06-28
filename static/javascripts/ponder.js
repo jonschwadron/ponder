eb5a7197258a54d4899deb9c990560d9cd11ecfa
jonschwadron <jonschwadron@gmail.com>
2015-06-28 22:43:54 +0000

    more bug fixes

---
+++ static/javascripts/ponder.js
@@ -0,0 +1,35 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('ponder', [
+      'ponder.config',
+      'ponder.routes',
+      'ponder.authentication',
+      'ponder.layout',
+      'ponder.routes',
+      'ponder.authentication'
+    ]);
+
+  angular
+    .module('ponder.routes', ['ngRoute']);
+
+  angular
+    .module('ponder.config', []);
+
+  angular
+   .module('ponder')
+   .run(run);
+
+  run.$inject = ['$http'];
+
+  /**
+  * @name run
+  * @desc Update xsrf $http headers to align with Django's defaults
+  */
+  function run($http) {
+    $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $http.defaults.xsrfCookieName = 'csrftoken';
+  } 
+
+})();
