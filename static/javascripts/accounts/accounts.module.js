5621a9694bba5804046a5903e4d54d86bc0acd89
jonschwadron <jonschwadron@gmail.com>
2015-06-29 05:05:24 +0000

    added user accounts

---
+++ static/javascripts/accounts/accounts.module.js
@@ -0,0 +1,15 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('ponder.accounts', [
+      'ponder.accounts.controllers',
+      'ponder.accounts.services'
+    ]);
+
+  angular
+    .module('ponder.accounts.controllers', []);
+
+  angular
+    .module('ponder.accounts.services', []);
+})();
