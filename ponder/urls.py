49481d192790e07217a958d16a54ad3efde40e4c
jonschwadron <jonschwadron@gmail.com>
2015-06-29 20:24:42 +0000

    small change

--- ponder/urls.py
+++ ponder/urls.py
@@ -17,11 +17,11 @@ accounts_router.register(r'posts', AccountPostsViewSet)
 
 urlpatterns = patterns(
     '',
-
     url(r'^api/v1/', include(router.urls)),
     url(r'^api/v1/', include(accounts_router.urls)),
     url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
 
     url(r'^.*$', IndexView.as_view(), name='index'),
+
 )
