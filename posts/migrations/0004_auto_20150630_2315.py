8e03c3f9c0e6e168c9f64ae821cf05447794c544
jonschwadron <jonschwadron@gmail.com>
2015-07-01 05:30:19 +0000

    update

---
+++ posts/migrations/0004_auto_20150630_2315.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('posts', '0003_auto_20150630_2310'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='created_at',
+            field=models.DateTimeField(auto_now_add=True),
+            preserve_default=True,
+        ),
+    ]
--- posts/models.py
+++ posts/models.py
@@ -7,6 +7,6 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    
     def __unicode__(self):
         return '{0}'.format(self.content)
